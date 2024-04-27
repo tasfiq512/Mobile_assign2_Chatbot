eea67a4aa00a4dde5c0dd001cd135a56241d1153
tasfiq512 <tasfiq512@gmail.com>
2024-04-27 23:56:41 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat Application',
+      title: 'Chat App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
